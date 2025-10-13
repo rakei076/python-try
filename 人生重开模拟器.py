0df74ecf5cdf570713ab7df6d4421dd8776b9ed1
rakei076 <lurenjialu2@gmail.com>
2025-10-14 00:56:53 +0900

    版本一

---
+++ 人生重开模拟器.py
@@ -0,0 +1,12 @@
+# 1. 下载项目代码
+git clone https://github.com/VickScarlet/lifeRestart.git my-project
+
+# 2. 进入目录安装依赖
+cd my-project
+pnpm install
+
+# 3. 转换XLSX表
+pnpm xlsx2json
+
+# 4. 启动本地开发服务器
+pnpm dev
---
+++ 实践/实践.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+print(dir(datetime))
+['MAXYEAR', 'MINYEAR', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'date', 'datetime', 'datetime_CAPI', 'sys', 'time', 'timedelta', 'timezone', 'tzinfo']
+now = datetime.now()
+print(now)                      # 2021-07-08 07:34:46.549883
+day = now.day                   # 8
+month = now.month               # 7
+year = now.year                 # 2021
+hour = now.hour                 # 7
+minute = now.minute             # 38
+second = now.second
+timestamp = now.timestamp()
+print(day, month, year, hour, minute)
+print('timestamp', timestamp)
+print(f'{day}/{month}/{year}, {hour}:{minute}')  # 8/7/2021, 7:38
+new_year = datetime(2020, 1, 1)
+print(new_year)      # 2020-01-01 00:00:00
+day = new_year.day
+month = new_year.month
+year = new_year.year
+hour = new_year.hour
+minute = new_year.minute
+second = new_year.second
+print(day, month, year, hour, minute) #1 1 2020 0 0
+print(f'{day}/{month}/{year}, {hour}:{minute}')  # 1/1/2020, 0:0
